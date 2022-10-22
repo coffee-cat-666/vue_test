117d90f20abfca2728115454e85e02ee160d3d72
laowang <hellogit@laowang.cn>
2022-10-22 10:49:25 +0800

    10/22

---
+++ 01_src_分析脚手架/components/School.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="demo">
+    <h2>学校名称:{{ schoolName }}</h2>
+    <h2>学校地址:{{ address }}</h2>
+    <button @click="showName">点我提示学校名</button>
+  </div>
+</template>
+
+<script>
+// 默认暴露
+export default {
+  name: "SchooL",
+  data() {
+    return {
+      schoolName: "武汉科技大学",
+      address: "武汉",
+    };
+  },
+  methods: {
+    showName() {
+      alert(this.schoolName);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ 01_src_分析脚手架/components/Student.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="demo">
+    <h2>学生姓名:{{ studentName }}</h2>
+    <h2>年龄:{{ age }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "StudneT",
+  data() {
+    return {
+      studnetName: "老王",
+      age: 19,
+    };
+  },
+};
+</script>
+
+<style>
+.demo {
+  color: pink;
+  font-size: 20px;
+}
+</style>
