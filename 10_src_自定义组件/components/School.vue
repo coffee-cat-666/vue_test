04f326ad8173bb32e638c7bccf119397244fbe4b
laowang <hellogit@laowang.cn>
2022-10-24 10:30:23 +0800

    10/24/

---
+++ 10_src_自定义组件/components/School.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="demo">
+    <h2>学校名称:{{ schoolName }}</h2>
+    <h2>学校地址:{{ address }}</h2>
+    <button @click="showName">点我提示学校名</button>
+    <button @click="sendByName">点我提示学校名</button>
+  </div>
+</template>
+
+<script>
+// 默认暴露
+export default {
+  name: "SchooL",
+  data() {
+    return {
+      schoolName: "武汉科技大学",
+      address: "武汉",
+    };
+  },
+  props: ["getByname"],
+  methods: {
+    showName() {
+      alert(this.schoolName);
+    },
+    sendByName() {
+      this.getByname(this.schoolName);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ 10_src_自定义组件/components/Student.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="demo">
+    <h2>学生姓名:{{ studnetName }}</h2>
+    <h2>年龄:{{ age }}</h2>
+    <button @click="sendStudentName">点我提示学生名</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "StudneT",
+  data() {
+    return {
+      studnetName: "老王",
+      age: 19,
+    };
+  },
+  methods: {
+    sendStudentName() {
+      // 省略props,直接用$emit来获取标签值
+      this.$emit(
+        "atguigu",
+        this.studnetName,
+        { key: "666", wang: "222" },
+        111,
+        333
+      );
+    },
+  },
+};
+</script>
+
+<style>
+.demo {
+  color: pink;
+  font-size: 20px;
+}
+</style>
